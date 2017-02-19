f8f5e4e0f761924a6e438f25920dab9ae4b33b3b
yanyongjun <yanyojun@foxmail.com>
2017-02-19 21:15:32 +0800

    Initial commit

---
+++ app/src/main/java/com/plbear/asynctasktest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.plbear.asynctasktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Test
+        setContentView(R.layout.activity_main);
+    }
+}
